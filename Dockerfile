aa72273d764417d2476b32ed13439bb7b5be8086
jbwexler <jbwexler@tutanota.com>
2023-11-01 10:09:40 -0700

    snapshot for flywheel support

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM mambaorg/micromamba:1.5.1
+USER root
 
 COPY --chown=$MAMBA_USER:$MAMBA_USER env.yaml /tmp/env.yaml
 RUN micromamba create -y -f /tmp/env.yaml && \
@@ -6,13 +7,15 @@ RUN micromamba create -y -f /tmp/env.yaml && \
 
 ENV PATH="/opt/conda/envs/openneuro-upload/bin:$PATH"
 RUN /opt/conda/envs/openneuro-upload/bin/npm install -g @openneuro/cli && \
-    rm -r ~/.npm
+	rm -r ~/.npm
 COPY requirements.txt /tmp/requirements.txt
 RUN /opt/conda/envs/openneuro-upload/bin/pip install --no-cache-dir -r /tmp/requirements.txt
 
-USER root
 ENV FLYWHEEL=/flywheel/v0
 RUN mkdir -p ${FLYWHEEL}
 COPY run.py ${FLYWHEEL}/run.py
-#USER $MAMBA_USER
+#COPY gitattributes.txt /flywheel/v0/work/
+COPY gitattributes.txt ${FLYWHEEL}/gitattributes.txt
 WORKDIR ${FLYWHEEL}
+
+ENTRYPOINT ["/flywheel/v0/run.py"]
--- run.py
+++ run.py
@@ -1,29 +1,42 @@
 #!/usr/bin/env python3
 
 import flywheel
+import flywheel_gear_toolkit
 import logging
 import datalad.api as dl
 from shutil import copyfile
-import subprocess
+import os.path
+import os
+import pygit2
 
-context = flywheel.GearContext()  # Get the gear context
-config = context.config           # from the gear context, get the config settings
+with flywheel_gear_toolkit.GearToolkitContext() as gtk_context:
+   # Setup basic logging
+   gtk_context.init_logging()
 
-#Initialize logging and set its level
-logging.basicConfig()
-log = logging.getLogger()
-log.setLevel(logging.INFO)
+   # Log the configuration for this job
+   gtk_context.log_config()
 
-git_config_path = context.get_input_path('git-config')
+
+config = gtk_context.config
+
+accession_number = config['accession_number']
+git_config_path = gtk_context.get_input_path('git-config')
 copyfile(git_config_path,'/root/.gitconfig')
-git_config_path = context.get_input_path('openneuro-config')
-copyfile(git_config_path,'/root/.openneuro')
+openneuro_config_path = gtk_context.get_input_path('openneuro-config')
+copyfile(openneuro_config_path,'/root/.openneuro')
 
-#print(dir(context))
-bids_path = context.download_project_bids()
+bids_path = gtk_context.download_session_bids()
 
-if config['new_dataset'] == 'yes': 
-    bashCommand = "cat config.json"
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
-    print(output)
+if config['new_dataset'] == 'yes':
+    ds_path = os.path.join(gtk_context.work_dir,'new_dataset')
+    dl.create(path=ds_path)
+    repo_obj = pygit2.Repository(ds_path)
+    copyfile(os.path.join('/flywheel/v0','gitattributes.txt'),os.path.join(ds_path,'.gitattributes'))
+    #copyfile(os.path.join(gtk_context.work_dir,'gitattributes.txt'),os.path.join(ds_path,'.gitattributes'))
+    repo_obj.config.set_multivar('credential.useHttpPath','', 'true')
+    repo_obj.config.set_multivar('credential.helper','','/path/to/openneuro git-credential')
+else:
+    ds_path = os.path.join(gtk_context.work_dir,accession_number)
+    dl.clone('https://github.com/OpenNeuroDatasets/%s.git' % accession_number)
+    repo_obj = pygit2.Repository(ds_path)
+    repo_obj.config.set_multivar('safe.directory','', '/flywheel/v0')
